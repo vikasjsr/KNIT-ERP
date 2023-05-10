d30f63993a1f8f8f5ced66831065a84fde937242
vikasjsr <vikasjaiswarskn@gmail.com>
2023-05-10 18:58:02 +0530

    Version 1.0

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const { connection } = await mongoose.connect('mongodb://localhost:27017/erp');
+  const { connection } = await mongoose.connect('mongodb+srv://vikasjaiswar2140:iamVikas%40123@cluster1.xlr3d08.mongodb.net/');
   console.log(`MongoDB connected with ${connection.host}`);
 };
